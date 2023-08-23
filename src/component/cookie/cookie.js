92c3e83829828f14e42cb69fbaf9d0e13ee9f78e
Alex Bond <bondarenko.aleksandr.25@gmail.com>
2023-08-23 15:19:00 +0600

    added padding-right

--- src/component/cookie/cookie.js
+++ src/component/cookie/cookie.js
@@ -1,15 +1,15 @@
-// export default (() => {
-//     const cookie = document.querySelector('.cookie');
-//     const childrenButtonsCookie = cookie.querySelectorAll('button');
+export default (() => {
+    const cookie = document.querySelector('.cookie');
+    const childrenButtonsCookie = cookie.querySelectorAll('button');
 
-//     const timerId = setTimeout(() => {
-//         cookie.classList.add('view-cookie');
-//         clearTimeout(timerId);
-//     }, 3000);
+    const timerId = setTimeout(() => {
+        cookie.classList.add('view-cookie');
+        clearTimeout(timerId);
+    }, 3000);
 
-//     childrenButtonsCookie.forEach((elem) => {
-//         elem.addEventListener('click', () => {
-//             cookie.classList.remove('view-cookie');
-//         });
-//     });
-// })();
+    childrenButtonsCookie.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            cookie.classList.remove('view-cookie');
+        });
+    });
+})();
