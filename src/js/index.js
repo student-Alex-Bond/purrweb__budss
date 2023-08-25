4a3b7bbf1baa50f8bf2b7a0e857ecf069895f00d
Alex Bond <bondarenko.aleksandr.25@gmail.com>
2023-08-25 07:08:37 +0600

    remake section benefit budss

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,5 @@ import '../component/cookie/cookie';
 import '../component/burger/burger';
 import '../component/form/form';
 import '../component/modal_success/modal_success';
-import '../component/benefit_budss/benefit_budss';
 import '../component/advantages/advantages';
 import '../component/tabs/tabs';
